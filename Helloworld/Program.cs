5e6b9b37e4062eb9dfe4d99db775f524c27b9ad3
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-02-08 07:52:20 +0530

    demo

---
+++ Helloworld/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace Helloworld
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // int num = 7;
+
+            //double num1 = 10.9;
+
+            //bool val = false;
+
+            //char singleCharacter = 'N';
+
+            //string name = "M. Nikhlesh";
+
+            //Console.WriteLine("Hi, Hello How are you?  ");
+
+            //Console.WriteLine("This is an Integer value" + num);
+            //Console.WriteLine("This is an double value" + num1);
+            //Console.WriteLine("This is an boolian value" + val);
+            //Console.WriteLine("This is an character value" + singleCharacter);
+            //Console.WriteLine("This is an string value" + name);
+
+            //string nameOfPerson = Console.ReadLine();
+
+            //Console.WriteLine("The Name is " + nameOfPerson);
+            //Console.WriteLine("The Name is {0} {1} {2} {3}" , nameOfPerson,num,num1,val);
+
+            //Console.WriteLine("print the username");
+            //string username = Console.ReadLine();
+
+            //int val = username.Length;
+            //Console.WriteLine("The number of character is " + val);
+
+            //if (username.Length > 3)
+            //{
+            //    Console.WriteLine("Hello {0}, How are you?", username);
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The name is not having minimum 3 characters");
+            //}
+
+            //Console.WriteLine("Enter the first number");
+            //string num1 = Convert.ToInt64(Console.ReadLine();
+
+            //Console.WriteLine("Enter the Second number");
+            //string num2 = Convert.ToInt64((Console.ReadLine());
+
+            //string num3 = num1 + num2;
+
+            //Console.WriteLine("The addition of two number" + num3);
+
+            /*Console.WriteLine("Enter the year");
+            int Year = Convert.ToInt32(Console.ReadLine());
+
+            if (Year.Length < 3 && Year.Length > 5)
+            {
+                if (Year % 400 == 0)
+                {
+                    Console.WriteLine("This is leap year : " + Year);
+                }
+                else if (Year % 100 == 0)
+                {
+                    Console.WriteLine("This is not leap year : " + Year);
+                }
+                else if (Year % 4 == 0)
+                {
+                    Console.WriteLine("This is leap year : " + Year);
+                }
+                else
+                {
+                    Console.WriteLine("This is not leap year : " + Year);
+                }
+            }
+            else
+            {
+                Console.WriteLine("enter 4 digit number");
+            }*/
+            //Username.DisplayUsername();
+            //Console.WriteLine(Username);
+
+            flipcoin.CalculatePercentage();
+            //Console.WriteLine(flipcoin());
+
+        }
+    }
+}
---
+++ Helloworld/Username.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    public class Username
+    {
+        public static void DisplayUsername()
+        {
+            Console.WriteLine("print the username");
+            string username = Console.ReadLine();
+
+            int val = username.Length;
+            Console.WriteLine("The number of character is " + val);
+
+            if (username.Length > 3)
+            {
+                Console.WriteLine("Hello {0}, How are you?", username);
+            }
+            else
+            {
+                Console.WriteLine("The name is not having minimum 3 characters");
+            }
+        }
+        
+          
+
+    }
+}
